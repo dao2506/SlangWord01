3ea4cbfce61f9ab5b49c068b3ac5dea2f630d988
Admins <ledao2506@gmail.com>
2022-11-24 22:56:44 +0700

    update: new construction for class Word

--- src/Word.java
+++ src/Word.java
@@ -3,4 +3,10 @@ import java.util.ArrayList;
 public class Word {
     String key;
     ArrayList<String> meaning;
+
+    public Word(String temp) {
+        //split by ' to get key
+
+        //split by | to get meanings
+    }
 }
