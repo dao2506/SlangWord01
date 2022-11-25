12adbab083c0341bf97af263c6e5bc3ed30b5703
Admins <ledao2506@gmail.com>
2022-11-25 23:29:49 +0700

    create java form

---
+++ src/DictionaryApp.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class DictionaryApp {
+    private JButton TranslateButton;
+    private JLabel Key;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
