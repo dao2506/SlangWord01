e8f9517b8372e50afa9a3fc4c2f4920ee594fbf9
Admins <ledao2506@gmail.com>
2022-11-25 20:37:58 +0700

    update: update construction for class Word

--- src/Word.java
+++ src/Word.java
@@ -2,11 +2,19 @@ import java.util.ArrayList;
 
 public class Word {
     String key;
-    ArrayList<String> meaning;
+    ArrayList<String> meaning = new ArrayList<String>();
 
-    public Word(String temp) {
+    public Word(String string) {
         //split by ' to get key
-
+        String[] key = string.split("'",2);
+        this.key = key[0];
         //split by | to get meanings
+        this.meaning.add(key[1]);
     }
+
+    public void display(){
+        System.out.println("Key: " + key);
+        System.out.println("Meaning" + meaning);
+    }
+
 }
