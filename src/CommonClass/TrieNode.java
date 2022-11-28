68ef2d361685d82a0c36ba62aad9c5449aae93e9
Admins <ledao2506@gmail.com>
2022-11-28 22:09:06 +0700

    Chỉnh sửa class TrieNode

--- src/CommonClass/TrieNode.java
+++ src/CommonClass/TrieNode.java
@@ -2,16 +2,14 @@ package CommonClass;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 // nguồn tham khảo: https://leduythuccs.github.io/2018-05-23-c-u-tr-c-d-li-u-trie/
 // Trong bài viết gốc tác giả dùng ngôn ngữ C++, Lê Phương Đào (sinh viên làm đồ án này) đã viết lại bằng java
 // và chỉnh sửa để phù hợp với bài toán cần xử lí hiện tại.
 public class TrieNode {
-    ArrayList<TrieNode> child = new ArrayList<TrieNode>(95);
-    Character cnt;
-    TrieNode() {
-        child.replaceAll(null);
-        cnt=0;
-    }
+    HashMap<Character, TrieNode> children;
+    Character key;
+    boolean isWord;
 }
 
